6ba5a0d264fea738202bc2a3b09f6b5eac3b6d16
jon-xu <40813452+jon-xu@users.noreply.github.com>
2020-08-20 15:38:01 +1000

    Add files via upload

--- stlearn/add.py
+++ stlearn/add.py
@@ -3,6 +3,4 @@ from .adds.add_positions import positions
 from .adds.parsing import parsing
 from .adds.add_lr import lr
 from .adds.annotation import annotation
-#from .adds.add_cpdb import cpdb
-from .adds.auto_annotate import auto_annotate
-from .adds.add_loupe_clusters import add_loupe_clusters
+from .adds.add_labels import labels
