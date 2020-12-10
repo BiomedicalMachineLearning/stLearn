6e4ee4839eff8b44a9994e155fc6dc90d93e726a
jon-xu <40813452+jon-xu@users.noreply.github.com>
2020-12-10 14:17:42 +1000

    Update pl.py

--- stlearn/pl.py
+++ stlearn/pl.py
@@ -3,7 +3,9 @@ from .plotting.cluster_plot import cluster_plot
 from .plotting.subcluster_plot import subcluster_plot
 from .plotting.microenv_plot import microenv_plot
 from .plotting.non_spatial_plot import non_spatial_plot
+from .plotting.deconvolution_plot import deconvolution_plot
 from .plotting import trajectory
 from .plotting.QC_plot import QC_plot
 from .plotting.cci_plot import het_plot
+from .plotting.cci_plot import violin_plot
 from .plotting.cci_plot import grid_plot
