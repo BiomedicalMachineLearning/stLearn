1a0a97c064a06cd68679334013b8bfa0bf9c0a05
jon-xu <40813452+jon-xu@users.noreply.github.com>
2020-12-10 14:15:50 +1000

    Add files via upload

--- stlearn/plotting/cci_plot.py
+++ stlearn/plotting/cci_plot.py
@@ -21,7 +21,7 @@ def het_plot(
     show_legend: bool = False,
     show_color_bar: bool = True,
     show_axis: bool = False,
-    dpi: int = 192,
+    dpi: int = 100,
     name: str = None,
     output: str = None,
     copy: bool = False,
@@ -66,8 +66,6 @@ def het_plot(
     -------
     Nothing
     """
-
-    plt.rcParams['figure.dpi'] = dpi
 
     colors = adata.uns[use_het].tolist()
 
@@ -128,7 +126,7 @@ def grid_plot(
     num_col: int = 10,
     vmin: float = None,
     vmax: float = None,
-    dpi: int = 192,
+    dpi: int = 100,
     name: str = None,
     output: str = None,
     copy: bool = False,
@@ -153,7 +151,6 @@ def grid_plot(
     Nothing
     """
 
-    plt.rcParams['figure.dpi'] = dpi
     plt.subplots()
 
     sns.heatmap(pd.DataFrame(np.array(adata.uns[use_het]).reshape(num_col, num_row)).T, vmin=vmin, vmax=vmax)
@@ -161,89 +158,5 @@ def grid_plot(
 
     if output is not None:
         plt.savefig(output + "/" + name + "_heatmap.pdf", dpi=dpi, bbox_inches='tight', pad_inches=0)
-
-    plt.show()
-
-
-def violin_plot(
-    adata: AnnData,
-    lr: str,
-    use_cluster: str = 'louvain',
-    dpi: int = 100,
-    name: str = None,
-    output: str = None,
-):
-    """ Plot the distribution of CCI counts within spots of each CCI clusters
-    Parameters
-    ----------
-    adata: AnnData          The data object to plot
-    lr: str                 The specified Ligand-Receptor pair to plot
-    use_cluster: str        The clustering results to use
-    dpi: bool               Dots per inch
-    name: str               Save as file name
-    output: str             Save to directory
-    Returns
-    -------
-    N/A
-    """
-    try:
-        violin = adata.obsm['lr_neighbours'][[lr]]
-    except:
-        sys.exit('Please run cci counting and clustering first.')
-    violin.columns = ['LR_counts']
-    violin['cci_cluster'] = adata.obs['lr_neighbours_' + use_cluster]
-    plt.rcParams['figure.dpi'] = dpi
-    sns.violinplot(x='cci_cluster', y='LR_counts', data=violin, orient='v')
-    if name is None:
-        name = use_cluster
-
-    if output is not None:
-        plt.savefig(output + "/" + name + ".pdf", dpi=dpi, bbox_inches='tight', pad_inches=0)
-
-    plt.show()
-    
-
-def stacked_bar_plot(
-    adata: AnnData,
-    use_annotation: str,
-    dpi: int = 100,
-    name: str = None,
-    output: str = None,
-):
-    """ Plot the proportion of cell types in each CCI cluster
-    Parameters
-    ----------
-    adata: AnnData          The data object to plot
-    use_annotation: str     The cell type annotation to be used in plotting
-    dpi: bool               Dots per inch
-    name: str               Save as file name
-    output: str             Save to directory
-    Returns
-    -------
-    N/A
-    """
-    sns.set()
-    try:
-        cci = adata.obs['lr_neighbours_louvain']
-    except:
-        sys.exit('Please run cci counting and clustering first.')
-    try:
-        label = adata.obs[use_annotation]
-    except:
-        sys.exit('spot cell type not found in data.obs[' + use_annotation + ']')
-    df = pd.DataFrame(0, index=sorted(set(cci)), columns=set(label))
-    for spot in cci.index:
-        df.loc[cci[spot], label[spot]] += 1
-
-    # From raw value to percentage
-    df2 = df.div(df.sum(axis=1), axis=0)
-    plt.rcParams['figure.dpi'] = dpi
-    df2.plot(kind='bar', stacked='True', legend=False)
-    plt.legend(loc='upper right', bbox_to_anchor=(1.5, 1), ncol=1)
-    if name is None:
-        name = use_annotation
-
-    if output is not None:
-        plt.savefig(output + "/" + name + ".pdf", dpi=dpi, bbox_inches='tight', pad_inches=0)
 
     plt.show()
