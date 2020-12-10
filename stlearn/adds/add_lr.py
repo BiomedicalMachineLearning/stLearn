5a81f2f5fae8bfb82aee9d1351fa99e67738a1e3
jon-xu <40813452+jon-xu@users.noreply.github.com>
2020-12-10 12:17:59 +1000

    Add files via upload

--- stlearn/adds/add_lr.py
+++ stlearn/adds/add_lr.py
@@ -7,8 +7,9 @@ import pandas as pd
 
 def lr(
     adata: AnnData,
-    cpdb_filepath: str = None,
+    db_filepath: str = None,
     sep: str = '\t',
+    source: str = 'connectomedb',
     copy: bool = False,
 ) -> Optional[AnnData]:
     """ Add significant Ligand-Receptor pairs into AnnData object
@@ -18,6 +19,7 @@ def lr(
     adata: AnnData          The data object to add L-R info into
     cpdb_filepath: str      The path to the CPDB results file
     sep: str                Separator of the CPDB results file
+    source: str             Source of LR database (default: connectomedb, can also support 'cellphonedb')
     copy: bool              Copy flag indicating copy or direct edit
 
     Returns
@@ -25,14 +27,20 @@ def lr(
     adata: AnnData          The data object that L-R added into
 
     """
-    cpdb = pd.read_csv(cpdb_filepath, sep=sep)
-    adata.uns["cpdb"] = cpdb
-    lr = cpdb['interacting_pair'].to_list()
-    lr2 = [i for i in lr if 'complex' not in i]
-    lr3 = [i for i in lr2 if ' ' not in i]
-    lr4 = [i for i in lr3 if i.count('_') == 1]
-    adata.uns['lr'] = lr4
-    print("cpdb results added to adata.uns['cpdb']")
-    print("Added ligand receptor pairs to adata.uns['lr'].")
+
+    if source == 'cellphonedb':
+        cpdb = pd.read_csv(db_filepath, sep=sep)
+        adata.uns["cpdb"] = cpdb
+        lr = cpdb['interacting_pair'].to_list()
+        lr2 = [i for i in lr if 'complex' not in i]
+        lr3 = [i for i in lr2 if ' ' not in i]
+        lr4 = [i for i in lr3 if i.count('_') == 1]
+        adata.uns['lr'] = lr4
+        print("cpdb results added to adata.uns['cpdb']")
+        print("Added ligand receptor pairs to adata.uns['lr'].")
+
+    elif source == 'connectomedb':
+        ctdb = pd.read_csv(db_filepath, sep=sep, quotechar='\"', encoding='latin1')
+        adata.uns['lr'] = (ctdb['Ligand gene symbol'] + '_' + ctdb['Receptor gene symbol']).values.tolist()
 
     return adata if copy else None
