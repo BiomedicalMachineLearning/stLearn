4a3a69b3c6dae91065faddafca85f2ede648cbcb
jon-xu <40813452+jon-xu@users.noreply.github.com>
2021-03-05 15:34:21 +1000

    Add files via upload
    comments updated

--- stlearn/adds/add_labels.py
+++ stlearn/adds/add_labels.py
@@ -9,16 +9,16 @@ def labels(
     adata: AnnData,
     label_filepath: str = None,
     index_col: int = 0,
-    sep: str = "\t",
+    sep: str = '\t',
     copy: bool = False,
 ) -> Optional[AnnData]:
-    """Add label transfer results into AnnData object
+    """ Add label transfer results into AnnData object
 
     Parameters
     ----------
     adata: AnnData          The data object to add L-R info into
     label_filepath: str     The path to the label transfer results file
-    sep: str                Separator of the CPDB results file
+    sep: str                Separator of the csv file
     copy: bool              Copy flag indicating copy or direct edit
 
     Returns
@@ -27,11 +27,9 @@ def labels(
 
     """
     labels = pd.read_csv(label_filepath, index_col=index_col, sep=sep)
-    adata.uns["label_transfer"] = labels.drop(
-        ["predicted.id", "prediction.score.max"], axis=1
-    )
-    adata.obs["predictions"] = labels["predicted.id"]
-    print("label transfer results added to adata.uns['label_transfer']")
-    print("predicted label added to adata.obs['predictions'].")
+    adata.uns["label_transfer"] = labels.drop(['predicted.id','prediction.score.max'], axis=1)
+    adata.obs['predictions'] = labels['predicted.id']
+    print("label transfer results added to adata.uns[\'label_transfer\']")
+    print("predicted label added to adata.obs[\'predictions\'].")
 
     return adata if copy else None
