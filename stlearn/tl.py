ad1187d511cb2eea8a08cf02274290d1e7f1fb83
Duy Pham <d.pham@imb19-014795.imb.uq.edu.au>
2021-03-18 15:12:32 +1000

    refactor: fix and clean the external funcs

--- stlearn/tl.py
+++ stlearn/tl.py
@@ -2,5 +2,3 @@ from .tools import clustering
 from .tools import enrichment_analysis
 from .tools import microenv
 from .tools.microenv import cci
-from .tools.microenv import SpatialDE
-from .tools.microenv import spatial_patterns
