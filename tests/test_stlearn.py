2dc16afda888da339fbb064fee928d8e4c062b71
Duy Pham <d.pham@imb19-014795.imb.uq.edu.au>
2021-03-19 17:58:16 +1000

    Initialize unittest

--- tests/test_stlearn.py
+++ tests/test_stlearn.py
@@ -13,21 +13,4 @@ from stlearn import cli
 class TestStlearn(unittest.TestCase):
     """Tests for `stlearn` package."""
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'stlearn.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+    
