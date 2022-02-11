ed7baa512d8b131530fde54e2b2b9b2d9e245118
bradbal <s4392586@student.uq.edu.au>
2022-02-11 10:43:04 +1000

    Comment tidying

--- stlearn/tools/microenv/cci/permutation.py
+++ stlearn/tools/microenv/cci/permutation.py
@@ -434,7 +434,7 @@ def permutation(
     # log back the original query
     adata.uns["lr"] = query_pair
 
-    ##### Negative Binomial fit - dosn't make sense, distribution not neg binom
+    #### Negative Binomial fit
     pvals, pvals_adj, log10_pvals, lr_sign = get_stats(
         scores, background, neg_binom, adj_method
     )
@@ -483,7 +483,7 @@ def get_stats(
     -------
     stats: tuple          Per spot pvalues, pvals_adj, log10_pvals_adj, lr_sign (the LR scores for significant spots).
     """
-    ##### Negative Binomial fit - dosn't make sense, distribution not neg binom
+    ##### Negative Binomial fit
     if neg_binom:
         # Need to make full background for fitting !!!
         background = np.array(list(background) + [0] * (total_bg - len(background)))
