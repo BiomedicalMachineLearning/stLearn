88e4031feab91d669a94c12b45af85b3af963633
bradbal <s4392586@student.uq.edu.au>
2022-11-17 11:03:54 +1000

    Trying njit

--- stlearn/tools/microenv/cci/het_helpers.py
+++ stlearn/tools/microenv/cci/het_helpers.py
@@ -255,7 +255,7 @@ def get_data_for_counting_OLD(adata, use_label, mix_mode, all_set):
     spot_bcs = adata.obs_names.values.astype(str)
     return spot_bcs, cell_data, neighbourhood_bcs, neighbourhood_indices
 
-@jit
+@njit
 def get_neighbourhoods_FAST(spot_bcs: np.array, spot_neigh_bcs: np.ndarray,
                             str_dtype: str):
     """Gets the neighbourhood information, njit compiled."""
