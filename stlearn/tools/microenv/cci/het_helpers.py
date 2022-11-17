054ee902f16c0761fc8143c1af21e06fd8c9fc8a
bradbal <s4392586@student.uq.edu.au>
2022-11-17 13:52:29 +1000

    Trying njit

--- stlearn/tools/microenv/cci/het_helpers.py
+++ stlearn/tools/microenv/cci/het_helpers.py
@@ -280,20 +280,24 @@ def get_neighbourhoods_FAST(spot_bcs: np.array, spot_neigh_bcs: np.ndarray,
                 neigh_bcs_sub.append( neigh_bc )
 
         #neigh_bcs_array = np.empty((len(neigh_bcs_sub)), str_dtype)
+        neigh_bcs_array = np.empty(len(neigh_bcs_sub),
+                                                     dtype=neigh_bcs_sub._dtype)
         neigh_indices = np.zeros((len(neigh_bcs_sub)), dtype=np.int64)
         for j, neigh_bc in enumerate(neigh_bcs_sub):
+            neigh_bcs_array[j] = neigh_bc
+
             for k, bc in enumerate(spot_bcs):
                 if neigh_bc==bc:
                     neigh_indices[j] = k
                     break
-                
+
             #neigh_indices[j] = np.where(spot_bcs == neigh_bc)[0][0]
             #neigh_bcs_array.append( neigh_bc )
 
         neighbours.append( neigh_indices )
         neighbourhood_indices.append( (i, neigh_indices) )
-        #neighbourhood_bcs.append( (spot_bcs[i], neigh_bcs_array) )
-        neighbourhood_bcs.append((spot_bcs[i], np.asarray(neigh_bcs_sub)))
+        neighbourhood_bcs.append( (spot_bcs[i], neigh_bcs_array) )
+        #neighbourhood_bcs.append((spot_bcs[i], np.asarray(neigh_bcs_sub)))
 
     return neighbours, neighbourhood_bcs, neighbourhood_indices
 
