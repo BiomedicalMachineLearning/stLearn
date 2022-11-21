221ba42bc2bb7b8c222cbb21db2a9a49dbf579bf
bradbal <s4392586@student.uq.edu.au>
2022-11-21 12:37:10 +1000

    PLaying with gridding optimisation.

--- stlearn/tools/microenv/cci/het.py
+++ stlearn/tools/microenv/cci/het.py
@@ -468,7 +468,7 @@ def count_grid(
 
     return adata
 
-@jit(parallel=True)
+@jit(parallel=True, forceobj=True, nopython=False)
 def grid_parallel(grid_coords: np.ndarray, xedges: np.array, yedges: np.array,
               n_row: int, n_col: int, xs: np.array, ys: np.array,
               cell_bcs: np.array,
