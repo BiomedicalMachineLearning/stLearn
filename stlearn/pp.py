242a7deaa10b83951a0859c2834d48b4a1dafcb5
Andrew Newman <andrewfnewman@gmail.com>
2025-06-10 17:17:24 +1000

    Oops.

--- stlearn/pp.py
+++ stlearn/pp.py
@@ -1,4 +1,4 @@
-from .image_preprocessing.feature_extractor import extract_feature
+from .image_preprocessing.feature_extractor import extract_feature, new_extract_feature
 from .image_preprocessing.image_tiling import tiling
 from .preprocessing.filter_genes import filter_genes
 from .preprocessing.graph import neighbors
