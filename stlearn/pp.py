63b4d746ca30c0bcd36fca4b7db27fdb3d3c97ba
Andrew Newman <andrewfnewman@gmail.com>
2025-06-12 14:13:11 +1000

    Add old tiling back.

--- stlearn/pp.py
+++ stlearn/pp.py
@@ -1,5 +1,5 @@
 from .image_preprocessing.feature_extractor import extract_feature, old_extract_feature
-from .image_preprocessing.image_tiling import tiling
+from .image_preprocessing.image_tiling import tiling, old_tiling
 from .preprocessing.filter_genes import filter_genes
 from .preprocessing.graph import neighbors
 from .preprocessing.log_scale import log1p, scale
@@ -12,6 +12,7 @@ __all__ = [
     "scale",
     "neighbors",
     "tiling",
+    "old_tiling",
     "extract_feature",
     "old_extract_feature",
 ]
