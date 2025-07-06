d7e8dc8221d77a879bc10fe8db5cd50c902e578c
Andrew Newman <andrewfnewman@gmail.com>
2025-07-07 06:44:06 +1000

    Add downloading the tutorial from google drive.

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,32 @@
 import os
 import sys
+import re
+import requests
+
 sys.path.insert(0, os.path.abspath(".."))
 import stlearn
+
+def download_gdrive_file(file_id, filename):
+    session = requests.Session()
+    url = f"https://docs.google.com/uc?export=download&id={file_id}"
+    response = session.get(url)
+
+    form_action_match = re.search(r'action="([^"]+)"', response.text)
+    if not form_action_match:
+        raise Exception("Could not find form action URL")
+    download_url = form_action_match.group(1)
+
+    params = {}
+    hidden_inputs = re.findall(
+        r'<input type="hidden" name="([^"]+)" value="([^"]*)"', response.text)
+    for name, value in hidden_inputs:
+        params[name] = value
+
+    response = session.get(download_url, params=params, stream=True)
+    with open(filename, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                file.write(chunk)
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -10,7 +35,6 @@ import stlearn
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 
 project = 'stLearn'
 copyright = '2022-2025, Genomics and Machine Learning Lab'
@@ -34,6 +58,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+def setup(app):
+    if not os.path.isdir("./tutorials"):
+        download_gdrive_file("1FNMzO4-KsHK8tPd8k5-sTiRS40S97Qs4", "tutorials.zip")
+        os.system("unzip tutorials.zip")
+    return
 
 html_theme = 'furo'
 html_static_path = ['_static']
