44447029e5135c741181364577a02131284741a2
lhock17 <levi.hockey@gmail.com>
2026-04-16 14:57:33 +1000

    fix get_interaction_matrix test

--- tests/tl/test_cci.py
+++ tests/tl/test_cci.py
@@ -279,6 +279,7 @@ class TestCCI(unittest.TestCase):
         # Get interaction matrix
         int_matrix = het.get_interaction_matrix(
             cell_data,
+            self.neighbourhood_indices,
             CELL_TYPE_LABELS,
             sig_bool,
             ligand_bool,
